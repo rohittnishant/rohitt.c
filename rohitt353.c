59fb9618ad53145b6fdcd9df52f859fbe1640ffd
rohittnishant <36066907+rohittnishant@users.noreply.github.com>
2018-03-05 16:05:26 +0530

    Create rohitt353.c

---
+++ rohitt353.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n,d,n1,n2,n3,n4;
+	printf("ENTER THE INPUT NUMBER\n");
+	scanf("%d",&n);
+if(d!=0)
+	{
+		n1=d%/100;
+		n2=d%100;
+		n3=n2/10;
+		n4=n2%10;
+	}
+	printf("THE NOS IN THE INPUT NUMBER IS\nd%d\n%d\n%d",n1,n3,n4);
+  getch();
+  return 0;
+  }
